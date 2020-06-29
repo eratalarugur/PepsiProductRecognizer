6a700615a028fcc7d103204f8a9c84e8eba6977f
ugureratalar <ugur.eratalar@adesso.com.tr>
2020-06-29 16:24:29 +0300

    Initial Commit

---
+++ PepsiProductRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PepsiProductRecognizer
+//
+//  Created by Ugur Eratalar on 29.06.2020.
+//  Copyright © 2020 Ugur Eratalar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
